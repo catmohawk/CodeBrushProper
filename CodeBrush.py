39a5f16a486ebd3d7a57ba9be5cacedd1eddeb1b
catmohawk <103516239+catmohawk@users.noreply.github.com>
2024-07-14 19:39:04 -0400

    Version 1.0
    This piece of software atleast in the current state MUST be run within a code editor. For example Visual Studio Code, or Notepad++
    
    SETUP INSTRUCTIONS
    
    To setup this ensure that the python extension is installed and functional  (Go online for more assistance)
            Next all you need to mess with is the Layers Each layer represents a row of image for your picture so in this case Layer_1 would = The first row of the software

---
+++ CodeBrush.py
@@ -0,0 +1,58 @@
+#Welcome to CodeBrush
+#The My Way of creating Character Art
+#--Version_01---
+
+Layer_01 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                                                               
+Layer_02 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                                                                                
+Layer_03 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_04 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_05 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_06 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_07 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_08 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_09 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_10 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_11 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_12 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_13 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_14 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_15 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_16 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_17 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_18 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_19 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_20 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_21 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_22 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_23 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+Layer_24 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                                                             
+
+RenderObject = f"""
+
+{Layer_01}
+{Layer_02} 
+{Layer_03} 
+{Layer_04} 
+{Layer_05} 
+{Layer_06} 
+{Layer_07} 
+{Layer_08} 
+{Layer_09} 
+{Layer_10} 
+{Layer_11} 
+{Layer_12} 
+{Layer_13} 
+{Layer_14} 
+{Layer_15} 
+{Layer_16} 
+{Layer_17} 
+{Layer_18} 
+{Layer_19} 
+{Layer_20} 
+{Layer_21} 
+{Layer_22} 
+{Layer_23} 
+{Layer_24} 
+
+"""
+print(RenderObject)
